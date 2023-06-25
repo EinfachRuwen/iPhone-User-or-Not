f56ce513cc9e229b400f4ddf84890d8d1932cf83
EinfachRuwen <mail@byzero.dev>
2023-06-25 11:13:47 +0000

    Optimierte Version des Python-Skripts: Prüfen, ob ein / bzw. mehrere Nutzer schon in der JSON-Datei mit dem gleichen Namen existieren

--- create-json.py
+++ create-json.py
@@ -1,6 +1,15 @@
 import json
 
-json_file = input("Geben Sie den Dateinamen der JSON-Datei ein: ")
+
+def find_users_by_name(users, name):
+    matching_users = []
+    for user in users:
+        if user["name"] == name:
+            matching_users.append(user)
+    return matching_users
+
+
+json_file = input("Gib den Dateinamen der JSON-Datei ein: ")
 
 with open(json_file, encoding='utf-8') as file:
     data = json.load(file)
@@ -9,15 +18,42 @@ max_id = max(data, key=lambda x: x["id"])["id"] if data else 0
 
 while True:
     name = input("Name: ")
+
+    matching_users = find_users_by_name(data, name)
+
+    if len(matching_users) > 1:
+        print("Es gibt mehrere Benutzer mit dem Namen '{}'.".format(name))
+        print("Folgende Benutzer wurden gefunden:")
+
+        for user in matching_users:
+            print("ID: {}".format(user["id"]))
+            print("Bild-URL: {}".format(user["picture"]))
+            print("Ist iPhone-Benutzer: {}".format(user["isiPhoneUser"]))
+            print()
+
+        proceed = input("Möchtest du trotzdem fortfahren? (y/n): ").lower()
+        if proceed != "y":
+            continue
+
+    elif len(matching_users) == 1:
+        existing_user = matching_users[0]
+        print("Ein Benutzer mit dem Namen '{}' existiert bereits:".format(name))
+        print("ID: {}".format(existing_user["id"]))
+        print("Bild-URL: {}".format(existing_user["picture"]))
+        print("Ist iPhone-Benutzer: {}".format(existing_user["isiPhoneUser"]))
+
+        proceed = input("Möchtest du trotzdem fortfahren? (y/n): ").lower()
+        if proceed != "y":
+            continue
+
     picture = input("Bild-URL: ")
+    picture = picture.replace("https://ruwen.is-from.space/", "https://ruwen.is-from.space/r/")
+
     is_iphone_user_input = input("Ist iPhone-Benutzer? (y/n): ").lower()
 
     is_iphone_user = is_iphone_user_input in ["y", "yes"]
 
     max_id += 1
-
-    # Aktualisieren der Bild-URL mit /r/ am Anfang
-    picture = "https://ruwen.is-from.space/r/" + picture.split("/")[-1]
 
     new_data = {
         "id": max_id,
